6afaa22e07ae1becd12dbe859fa674890c119a30
sivyy75 <162481765+sivyy75@users.noreply.github.com>
2026-01-01 16:09:41 +0300

    Update operaproxy.sh

--- operaproxy.sh
+++ operaproxy.sh
@@ -45,6 +45,13 @@ STOP=89
 PROG=/usr/bin/opera-proxy
 start_service() {
         procd_open_instance
+        procd_set_param command "$PROG" -verbosity 50 -bind-address 0.0.0.0:18079
+        procd_set_param stdout 1
+        procd_set_param stderr 1
+        procd_set_param respawn ${respawn_threshold:-3600} ${respawn_timeout:-5} ${respawn_retry:-5}
+        procd_close_instance
+
+        procd_open_instance
         procd_set_param command "$PROG" -verbosity 50 -bind-address 0.0.0.0:18080
         procd_set_param stdout 1
         procd_set_param stderr 1
@@ -53,13 +60,6 @@ start_service() {
 
         procd_open_instance
         procd_set_param command "$PROG" -verbosity 50 -country AM -bind-address 0.0.0.0:18081
-        procd_set_param stdout 1
-        procd_set_param stderr 1
-        procd_set_param respawn ${respawn_threshold:-3600} ${respawn_timeout:-5} ${respawn_retry:-5}
-        procd_close_instance
-
-        procd_open_instance
-        procd_set_param command "$PROG" -verbosity 50 -country AS -bind-address 0.0.0.0:18082
         procd_set_param stdout 1
         procd_set_param stderr 1
         procd_set_param respawn ${respawn_threshold:-3600} ${respawn_timeout:-5} ${respawn_retry:-5}
