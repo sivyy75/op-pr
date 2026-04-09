8a2d4dfb75cd3fa3568f29401fbd7641d9dbaef0
sivyy75 <162481765+sivyy75@users.noreply.github.com>
2026-04-09 13:15:40 +0400

    Update operaproxy.sh

--- operaproxy.sh
+++ operaproxy.sh
@@ -21,7 +21,7 @@ esac
 
 OPERA_BIN="/usr/bin/opera-proxy"
 # 🔥 ИСПРАВЛЕНО: убраны пробелы в URL!
-OPERA_URL="https://github.com/Snawoot/opera-proxy/releases/download/v1.15.0/$FILE"
+OPERA_URL="https://github.com/Alexey71/opera-proxy/releases/tag/v1.16.0/$FILE"
 
 # Остановка старого процесса
 killall opera-proxy 2>/dev/null || true
