c2dd84f4bd462e57c434dfcb3f387975367386f5
sivyy75 <162481765+sivyy75@users.noreply.github.com>
2026-05-09 12:14:26 +0400

    Update operaproxy.sh

--- operaproxy.sh
+++ operaproxy.sh
@@ -19,9 +19,9 @@ case "$ARCH" in
     *)             fail "Неизвестная архитектура: $ARCH" ;;
 esac
 
-OPERA_BIN="/usr/bin/opera-proxy"
+OPERA_BIN="/usr/bin/opera-proxy_Alexey71"
 # 🔥 ИСПРАВЛЕНО: убраны пробелы в URL!
-OPERA_URL="https://github.com/Alexey71/opera-proxy/releases/tag/v1.16.0/$FILE"
+OPERA_URL="https://github.com/Alexey71/opera-proxy/releases/latest/$FILE"
 
 # Остановка старого процесса
 killall opera-proxy 2>/dev/null || true
